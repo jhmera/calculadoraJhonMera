5d9eb4f99791de803125e736018d8309cc87c64f
jhmera <jjmebur@gmail.com>
2015-02-19 11:28:34 -0500

    estructura de la calculadora y de la funcion en js

---
+++ logica.js
@@ -0,0 +1,25 @@
+var numSum1, numSum2, numSum3;
+var numRes1, numRes2, numRes3;
+var numMul1, numMul2, numMul3;
+var numDiv1, numDiv2, numDiv3;
+var resultadoOperacion;
+function sumar (numSum1, numSum2)
+{
+
+
+}
+
+function restar (numRes1, numRes2)
+{
+
+}
+
+function multiplicar()
+{
+
+}
+
+function dividir()
+{
+	
+}
