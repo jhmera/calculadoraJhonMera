69cd6576117d9e3a797a20520574e8cb3e6c1758
jhmera <jjmebur@gmail.com>
2015-02-25 07:53:09 -0500

    correccion

--- logica.js
+++ logica.js
@@ -1,43 +1,76 @@
-var numSum1, numSum2;
-var numRes1, numRes2;
-var numMul1, numMul2;
-var numDiv1, numDiv2;
-var resultadoOperacion;
-var operacion = 0;
-function sumar (numSum1, numSum2)
-{
+  var num1=0;
+  var num2=0; 
+  var mostrado=""; 
+  comprobador=0; 
+  operador=0; 
 
-
-}
-
-function restar (numRes1, numRes2)
-{
-
-}
-
-function multiplicar()
-{
-
-}
-
-function dividir()
-{
-	
-}
-
-
-
-
-function seleccionarOperacion(n)
+function seleccionar(a)
 { 
+	switch (a){ 
+    case 1: 
+      operador=1 
+  
+      document.getElementById("txtResultado").value="+";
+      break; 
+    case 2: 
+      operador=2 
+  
+      document.getElementById("txtResultado").value="-";
+      break; 
+    case 3: 
+      operador=3 
+  
+      document.getElementById("txtResultado").value="*";
+      break; 
+    case 4: 
+      operador=4 
+        document.getElementById("txtResultado").value="/";
+      break; 
+    default: 
+    break; 
+  } 
+  mostrado=""; 
+  comprobador=1; 
 } 
 
-function darNumero(n)
+function darNumero(a)
 { 
+	mostrado=mostrado+a; 
+  
+  document.getElementById("txtResultado").value=""+mostrado;
+
+  if(comprobador==0){ 
+    num1=parseFloat(mostrado); 
+  }
+  else if(comprobador==1){ 
+    num2=parseFloat(mostrado); 
+} 
   
 } 
 
 function resultado()
 { 
+  switch (operador){ 
+    case 1: 
+      
+      document.getElementById("txtResultado").value=num1+num2;
+      break; 
+    case 2: 
+      
+      document.getElementById("txtResultado").value=num1-num2;
+      break; 
+    case 3: 
+     
+      document.getElementById("txtResultado").value=num1*num2;
+      break; 
+    case 4: 
+     
+      document.getElementById("txtResultado").value=num1/num2;
+      break; 
+      default: 
+    break;  
+  } 
   
+  num1=parseFloat(document.getElementById("txtResultado").value); 
+  mostrado=""; 
 } 
